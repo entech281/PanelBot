ba5f2a03eabe1c9523794d0be5a4bac1b13d1a8b
John-Molfetner <jtmolfenter07@gmail.com>
2024-02-08 20:04:58 -0500

    factory defaults

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -14,7 +14,6 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import frc.robot.utilities.MovingAverage;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
